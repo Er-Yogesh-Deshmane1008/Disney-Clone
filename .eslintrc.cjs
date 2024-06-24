bfff45dc8ce7974e0ea3f927df4d54506e622dda
[Er-Yogesh-Deshmane1008] <yogeshsdeshmane@gmail.com>
2024-06-24 22:15:46 +0530

    Update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,3 +18,46 @@ module.exports = {
     ],
   },
 }
+
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//   },
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:react/recommended',
+//     'plugin:react-hooks/recommended',
+//     'plugin:jsx-a11y/recommended',
+//     'plugin:import/errors',
+//     'plugin:import/warnings',
+//     'plugin:import/react',
+//   ],
+//   parserOptions: {
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//     ecmaVersion: 'latest',
+//     sourceType: 'module',
+//   },
+//   plugins: [
+//     'react',
+//     'react-hooks',
+//     'jsx-a11y',
+//     'import',
+//   ],
+//   settings: {
+//     react: {
+//       version: 'detect',
+//     },
+//     'import/resolver': {
+//       node: {
+//         extensions: ['.js', '.jsx', '.ts', '.tsx'],
+//       },
+//     },
+//   },
+//   rules: {
+//     'react/react-in-jsx-scope': 'off',
+//   },
+// };
+
